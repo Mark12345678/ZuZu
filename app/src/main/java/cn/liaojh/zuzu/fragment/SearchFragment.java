845bf4b1020256797bd722db799b9d5c8bbc85e6
liaojh <1095936979@qq.com>
2016-12-26 23:58:52 +0800

    用户修改发布物品信息（1）

--- app/src/main/java/cn/liaojh/zuzu/fragment/SearchFragment.java
+++ app/src/main/java/cn/liaojh/zuzu/fragment/SearchFragment.java
@@ -1,10 +1,11 @@
 package cn.liaojh.zuzu.fragment;
 
 
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -15,32 +16,22 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.cjj.MaterialRefreshLayout;
 import com.google.gson.reflect.TypeToken;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
-import com.squareup.okhttp.Response;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import cn.liaojh.zuzu.Contans;
 import cn.liaojh.zuzu.GoodsDetailActivity;
 import cn.liaojh.zuzu.R;
-import cn.liaojh.zuzu.SearchActivity;
 import cn.liaojh.zuzu.ZuZuApplication;
 import cn.liaojh.zuzu.adapter.HomeAdapter;
 import cn.liaojh.zuzu.bean.Goods;
 import cn.liaojh.zuzu.bean.Page;
-import cn.liaojh.zuzu.http.OkHttpHelper;
-import cn.liaojh.zuzu.http.SpotsCallBack;
 import cn.liaojh.zuzu.utils.Pager;
 import cn.liaojh.zuzu.utils.PagerBuilder;
 import cn.liaojh.zuzu.widget.MyToolBar;
@@ -101,12 +92,27 @@ public class SearchFragment extends Fragment{
         if(type_fragment == Contans.SEARCH_GOODS) {
             initData(title);
         }else if(type_fragment == Contans.RELEASE_GOODS) {
-            initReleaseGoods();
+            if(ZuZuApplication.getInstance().getUser() != null){
+                initReleaseGoods(ZuZuApplication.getInstance().getUser().getId());
+            }
         }
-        
+
     }
 
-    private void initReleaseGoods() {
+    private void initReleaseGoods(int userId) {
+
+        PagerBuilder builder = new PagerBuilder();
+        builder.setUrl(Contans.API.USERRELEASE);
+        builder.setCanLoadMore(true);
+        builder.setRefreshLayout(mRefreshLayout);
+        builder.setOnPageListener(new loadMessage());
+        builder.setPageSize(5);
+        builder.putParam("usetId",userId);
+        builder.build(getContext(),new TypeToken<Page<Goods>>(){}.getType());
+
+        Pager pager = new Pager(builder);
+        pager.request();
+
     }
 
     public void initData(String key){
@@ -135,18 +141,28 @@ public class SearchFragment extends Fragment{
 
             mAdapter = new HomeAdapter(getActivity(),datas);
             mRecyclerView.setAdapter(mAdapter);
-            mAdapter.setOnItemClickListener(new cn.liaojh.zuzu.adapter.BaseAdapter.OnItemClickListener() {
-                @Override
-                public void onClick(View view, int position) {
-                    Intent intent = new Intent();
-                    Bundle bundle = new Bundle();
-                    bundle.putSerializable("goods",datas.get(position));
-                    intent.putExtras(bundle);
-                    intent.setClass(getActivity(), GoodsDetailActivity.class);
-                    startActivity(intent);
+            if(type_fragment == Contans.SEARCH_GOODS){
+                mAdapter.setOnItemClickListener(new cn.liaojh.zuzu.adapter.BaseAdapter.OnItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position) {
+                        Intent intent = new Intent();
+                        Bundle bundle = new Bundle();
+                        bundle.putSerializable("goods",datas.get(position));
+                        intent.putExtras(bundle);
+                        intent.setClass(getActivity(), GoodsDetailActivity.class);
+                        startActivity(intent);
 
-                }
-            });
+                    }
+                });
+            }else if(type_fragment == Contans.RELEASE_GOODS){
+                mAdapter.setOnItemClickListener(new cn.liaojh.zuzu.adapter.BaseAdapter.OnItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position) {
+                        initReleaseSelect(datas.get(position));
+
+                    }
+                });
+            }
 
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
 
@@ -174,6 +190,57 @@ public class SearchFragment extends Fragment{
     }
 
 
+    public void initReleaseSelect(final Goods goods){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("干哈？");
+        builder.setIcon(R.drawable.default_head);
+        builder.setCancelable(false);  //不响应back按钮
+        builder.setItems(new String[]{"查看物品详情","添加授权用户","删除物品","取消选择图片"}, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case 0:      //查看物品详情
+                        dialog.dismiss();
+                        opeanMasterGoodsDetail(goods);
 
+                        break;
+
+                    case 1:      //添加授权用户
+                        dialog.dismiss();
+
+                        break;
+
+                    case 2:      //删除添加
+                        dialog.dismiss();
+                        break;
+
+                    case 3:      //取消添加
+                        dialog.dismiss();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        });
+        //显示对话框
+        builder.create().show();
+    }
+
+        public void opeanMasterGoodsDetail(Goods  goods){
+            MasterGoodsDetailFragment fragment = new MasterGoodsDetailFragment();
+            FragmentManager fmanger =getFragmentManager();
+            //开启一个事务
+            FragmentTransaction ftran =fmanger.beginTransaction();
+            //往Activity中添加fragment
+            ftran.add(R.id.meater_search,fragment);
+            //创建一个bundle对象，往里面设置参数
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("goods",goods);
+            //吧bundle当住参数，设置给fragment
+            fragment.setArguments(bundle);
+            ftran.addToBackStack("物主物品详情");
+            ftran.commit();
+        }
 
 }
